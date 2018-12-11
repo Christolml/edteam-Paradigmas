f942ac31901820f42b8759cb0ef4184c6efdf8a8
christolml <christo_9722@hotmail.com>
2018-12-11 16:18:04 -0600

    Continuando con this

--- 02-clase/5_this.js
+++ 02-clase/5_this.js
@@ -71,6 +71,92 @@
     alo objeto que contiene el método, sin importar si el método es añadido luego
     de haber sido creado el objeto, o si es una función que retorna un objeto. */
 
+})(console.log);
+
+((c) => {
+    // 2. Asignación explícita:
+        // Desde ES5 cuando deseamos explícitamente referenciar this contamos con 3 métodos
+        // call, apply y bind
+    c('*************Asignación de this explícita*********************')
+
+    const nombrar = function(f1, f2, f3) {
+        c(`${this.nombre} es el lenguaje Front end de la Web u tiene librerías y 
+frameworks muy poderosos como: ${f1}, ${f2} y ${f3}`)
+    }
+
+    const lenguaje = {
+        nombre: 'Javascript',
+        version: 6
+    }
+
+    let frameworks = ['Angular', 'React', 'Vue.js']
+
+    // call: permite definir a que va a hacer referencia this en su primer parámetro
+    // y los parámetros siguientes son los que recibe la función.
+    nombrar.call(lenguaje, frameworks[0], frameworks[1], frameworks[2])
+
+    // apply: permite referenciar this en el primer parámetro, pero este nos permite
+    // pasar un array como los parámetros de la función
+    nombrar.apply(lenguaje, frameworks)
+
+    // bind: devuelve una función, en dónde this hace referencia al objeto que pasamos
+    // en su parametro
+    const frameworksJS = nombrar.bind(lenguaje, frameworks[0], frameworks[1], frameworks[2])
+    frameworksJS()
+
+
+})(console.log);
+
+
+
+((c) => {
+    // 3. Asignación con new
+        // Cuando invocamos this en un constructor, éste hace referencia al objeto que
+        // se ha instanciado
+    c('*************Asignación de this con new*********************')
+
+    let Framework = function (nombre, url, lenguaje) {
+        this.nombre = nombre
+        this.url = url
+        this.lenguaje = lenguaje
+    }
+
+    const react = new Framework('React', 'https://facebook.github.io/react/', 'Javascript'),
+        vue = Object.create(Framework)
+        vue.nombre = 'Vue.js'
+        c(react, vue)
+})(console.log);
+
+
+
+((c) => {
+    //4. Asignación Global
+        // Uno de los grandes errores con this es que cuando no se tiene una
+        // referencia al objeto que representa this este hace referencia al 
+        // objeto global:
+            // window en los navegadores
+            // global en Node.js
+    
+    c('*************Asignación de this con Global*********************')
+    const dimeUnFramework = function () {
+        c(this.nombre)
+    }
+
+    dimeUnFramework()
+    // variable global, porque antes de nombre no le estoy poniendo let, var, const y al no ponerle eso
+    // antes a nombre me lo cuelga como global
+    // nombre = 'Angular'
+    window.nombre = 'Angular'
+    dimeUnFramework()
+
+
+
+})(console.log);
+
+
+((c) => {
+    // 5. Arrow functions
+    c('*************Arrow functions y el problema de this*********************')
 
 
 
